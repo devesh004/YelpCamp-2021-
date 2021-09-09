38f4fc9c7e09c51f1ab13c1ea3c1fb027fb7b795
Devesh Shakya <06deveshshakya@gamil.com>
2021-09-09 22:00:11 +0530

    improved seeding

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,7 +1,3 @@
-// if (process.env.NODE_ENV !== "production") {
-//     require('dotenv').config();
-// }
-// const mapToken = process.env.MAPBOX_TOKEN;
 
 mapboxgl.accessToken = mapToken;
 const allCampgrounds = { features: JSON.parse(campgrounds) }
